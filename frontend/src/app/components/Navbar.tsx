b1bc9a845171cb5db72adf7ed7dcf738b1266cef
Pedro Casseb <casseb.phcc@gmail.com>
2026-05-16 09:23:19 -0300

    update navbar/v2

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,10 +1,38 @@
-import { buttonVariants } from "@/components/ui/button";
+"use client";
+
+import { Button, buttonVariants } from "@/components/ui/button";
 import { ScanFace } from "lucide-react";
 import Link from "next/link";
 import Logo from "../../../public/logo.png";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function Navbar() {
+    const router = useRouter();
+
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    const isLogged = !!localStorage.getItem("token");
+
+    function handleLogout() {
+        localStorage.removeItem("token");
+
+        localStorage.removeItem("user");
+
+        router.push("/login");
+
+        router.refresh();
+    }
+
     return (
         <header className="fixed z-50 top-0 left-0 right-0 mt-4 mx-auto w-[95%] sm:w-[90%] lg:w-[85%] xl:w-300 flex items-center justify-between py-4 px-6 sm:px-8 rounded-xl border border-zinc-800/80 bg-transparent backdrop-blur-sm">
             <Link
@@ -14,23 +42,33 @@ export function Navbar() {
                 <Image src={Logo} alt="My Car Hub" className="w-10" />
                 My Car Hub
             </Link>
+
             <div className="flex items-center gap-6">
-                <Link
-                    className="text-zinc-400 text-sm hover:text-white duration-300"
-                    href="/login"
-                >
-                    Log in
-                </Link>
-                <Link
-                    className={buttonVariants({
-                        variant: "default",
-                        size: "lg",
-                    })}
-                    href="/register"
-                >
-                    <ScanFace />
-                    Register
-                </Link>
+                {isLogged ? (
+                    <Button onClick={handleLogout} size="lg">
+                        Logout
+                    </Button>
+                ) : (
+                    <>
+                        <Link
+                            className="text-zinc-400 text-sm hover:text-white duration-300"
+                            href="/login"
+                        >
+                            Log in
+                        </Link>
+
+                        <Link
+                            className={buttonVariants({
+                                variant: "default",
+                                size: "lg",
+                            })}
+                            href="/register"
+                        >
+                            <ScanFace />
+                            Register
+                        </Link>
+                    </>
+                )}
             </div>
         </header>
     );
